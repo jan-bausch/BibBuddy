72c0ebf33ccc1b4d098e60064fe28a6762106f74
Jan F. Bausch <mail@jfbausch.de>
2019-11-15 19:52:06 +0100

    Add basic endpoints with hardcoded data

--- backend/__init__.py
+++ backend/__init__.py
@@ -6,10 +6,33 @@ from flask import jsonify
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return jsonify(
         version="1.0.0"
     )
+
+@app.route("/calendar")
+def calendar():
+    return jsonify({
+        "groups": [
+            {
+                "id": "939",
+                "description": "Mathe II für Informatiker",
+                "date": "2019-12-17",
+                "start": 3,
+                "end": 4,
+                "members": 3
+            },
+            {
+                "id": "989",
+                "description": "GL1",
+                "date": "2019-12-18",
+                "start": 9,
+                "end": 11,
+                "members": 1
+            },
+        ]
+    })
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
