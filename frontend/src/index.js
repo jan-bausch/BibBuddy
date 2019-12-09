c345ce40597d63a4dd068ac7b68f846087b191fb
Jan F. Bausch <mail@jfbausch.de>
2019-12-09 15:55:19 +0100

    Add docker-compose with hot-reloading

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import GroupList from './components/GroupList';
+import Logo from '../static/logo.svg'
 
 class App extends Component {
    render() {
@@ -8,6 +9,7 @@ class App extends Component {
          <section className="section hero is-primary">
             <div className="hero-body">
                <div className="container">
+                  <img src={Logo} />
                   <h1 className="title">BibBuddy</h1>
                   <h2 className="subtitle">Finde deine Lerngruppe</h2>
                </div>
