dfe41997875367e6a9d6bece2ee14ebb556e1278
Jan F. Bausch <mail@jfbausch.de>
2020-01-01 17:40:05 +0100

    Style faq

--- frontend/src/components/FAQ.js
+++ frontend/src/components/FAQ.js
@@ -13,7 +13,7 @@ export default class FAQ extends Component {
                   </span>
                   Was ist BibBuddy?
                </strong>
-               <p>BibBuddy ist ein Uniprojekt im Rahmen des Praktikum <a href="https://www.edutec.guru/teaching/">Educational Technologies</a> in der Goethe Universität. Das Projekt soll Lernen mithilfe von Technologie unterstützen.</p>
+               <p>BibBuddy ist ein Uniprojekt im Rahmen des Praktikum <a className="has-text-primary" href="https://www.edutec.guru/teaching/">Educational Technologies</a> in der Goethe Universität. Das Projekt soll Lernen mithilfe von Technologie unterstützen.</p>
                <br/>
                <strong>
                   <span class="icon has-text-primary">
@@ -21,7 +21,7 @@ export default class FAQ extends Component {
                   </span>
                   Wo finde ich die Lernräume?
                </strong>
-               <p>Die Gruppenräume befinden sich in der <a href="https://www.ub.uni-frankfurt.de/bruw/">Bibliothek Recht und Wirtschaft</a> auf dem Campus Westend.</p>
+               <p>Die Gruppenräume befinden sich in der <a className="has-text-primary" href="https://www.ub.uni-frankfurt.de/bruw/">Bibliothek Recht und Wirtschaft</a> auf dem Campus Westend.</p>
                <br/>
                <strong>
                   <span class="icon has-text-primary">
@@ -29,7 +29,7 @@ export default class FAQ extends Component {
                   </span>
                   Welche Daten werden gespeichert?
                </strong>
-               <p>Personenbezogene Daten werden nicht auf dem Server gespeichert, sondern nur im <a href="https://support.mozilla.org/de/kb/cookies-informationen-websites-auf-ihrem-computer">Browser-Cookie</a>. Informationen über Lerngruppen werden nach dem Treffen gelöscht.</p>
+               <p>Personenbezogene Daten werden nicht auf dem Server gespeichert, sondern nur im <a className="has-text-primary" href="https://support.mozilla.org/de/kb/cookies-informationen-websites-auf-ihrem-computer">Browser-Cookie</a>. Informationen über Lerngruppen werden nach dem Treffen gelöscht.</p>
                <br/>
                <strong>
                   <span class="icon has-text-primary">
@@ -37,7 +37,7 @@ export default class FAQ extends Component {
                   </span>
                   Wo finde ich weitere Informationen?
                </strong>
-               <p>Informationen über BibBuddy und der Quellcode des Projekts sind auf <a href="https://github.com/jfbausch/BibBuddy">Github</a>.</p>
+               <p>Informationen über BibBuddy und der Quellcode des Projekts sind auf <a className="has-text-primary" href="https://github.com/jfbausch/BibBuddy">Github</a>.</p>
             </div>
             <div className="column">
                <figure className="image is-4by3">
